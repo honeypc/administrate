fc63e704dce33c271d1c1ade53ed7de1bb4e311f
Phuoc Tu <tuquangphuoc@gmail.com>
2023-09-02 13:38:13 +0700

    Add Dropdown, Enum, Toggle fields

---
+++ lib/administrate/field/enum.rb
@@ -0,0 +1,11 @@
+require "select"
+
+module Administrate
+  module Field
+    class Enum < Administrate::Field::Select
+      def selectable_options
+        resource.class.send(name).options
+      end
+    end
+  end
+end
